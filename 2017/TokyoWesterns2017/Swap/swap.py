90a57cacb5d062efe9bc2bd3346cdc6e4bf23f5e
bu6hunt3r <Felix1Koch@gmail.com>
2017-09-10 13:33:09 +0200

    Changed swap.py in tokyoWesterns

--- 2017/TokyoWesterns2017/Swap/swap.py
+++ 2017/TokyoWesterns2017/Swap/swap.py
@@ -2,12 +2,10 @@
 
 from pwn import *
 
-context(arch="amd64", os="linux", bits=64, log_level="INFO", terminal=["tmux", "splitw", "-h"])
-global local=True
-global remote=False
-e=ELF("./swap")
+#e=ELF("./swap")
 
 def conn():
+    context(arch="amd64", os="linux", bits=64, log_level="INFO", terminal=["tmux", "splitw", "-h"])
     return process(["./swap"], env={'LD_PRELOAD':"./libc.so.6"}) if local else remote('pwn1.chal.ctf.westerns.tokyo', 19937)
 
 def send(s):
@@ -18,14 +16,18 @@ def swap(addr1, addr2):
     print r.recvuntil("choice:\n")
     send(str(1))
     print r.recvuntil('Please input 1st addr')
-	send(str(address1))
-	print r.recvuntil('Please input 2nd addr')
-	send(str(address2))
-	print r.recvuntil('Your choice: \n')
-	send('2')
+    send(str(address1))
+    print r.recvuntil('Please input 2nd addr')
+    send(str(address2))
+    print r.recvuntil('Your choice: \n')
+    send('2')
 
 def main():
+    local=True
     r=conn()
+    pid=util.proc.pidof(r)[0]
+    util.proc.wait_for_debugger(pid)
+    print "[DEBUG]: ",str(pid)
 
 if __name__=="__main":
     main()
