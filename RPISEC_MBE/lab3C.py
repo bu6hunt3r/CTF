e7183112d36b4dbe0a29c3f0bbed78a6f796abb2
bu6hunt3r <Felix1Koch@gmail.com>
2017-09-23 19:13:11 +0200

    Removed lab3C.py

--- RPISEC_MBE/lab3C.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from pwn import *
-
-context(os="linux", arch="i386", bits=32, log_level="INFO")
-
-s=ssh(user="lab3C", password="lab03start", host="192.168.13.101")
-s.close()
