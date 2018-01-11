71fc0ad4b3eb40c250d9ab09a0fa50e67e07fb4c
bu6hunt3r <Felix1Koch@gmail.com>
2018-01-11 11:46:58 +0100

    Changed solve.py

--- 2017/34C3/solve.py
+++ 2017/34C3/solve.py
@@ -1,4 +1,5 @@
 from pwn import *
+from heapq import *
 
 with open("./primepwn",'rb') as f:
     data=f.read(0xa70)
@@ -16,7 +17,7 @@ def gen_primes(lower, upper):
 
 PRIMES=gen_primes(2,255)
 
-def gen_byte_generator():
+def gen_byte_generators():
     res={}
     for i in range(256):
         for a in range(len(PRIMES)):
@@ -58,37 +59,75 @@ cpu=CPU()
 def and_eax(n):
     global cpu
     cpu.eax &= n
-    return b"\x25"+p32(n)
+    return b'\x25' + p32(n)
 
 def xor_eax(n):
     global cpu
-    cpu.eax &= n
-    return b"\x35"+p32(n)
+    cpu.eax ^= n
+    return b'\x35' + p32(n)
 
 def zero_eax():
-    return and_eax(0x02020202)+and_eax(0x05050505)
+    return and_eax(0x02020202) + and_eax(0x05050505)
 
 def xchg_eax_edi():
     global cpu
-    cpu.eax, cpu.edi=cpu.edi, cpu.eax
-    return b"\x97"
+    cpu.eax, cpu.edi = cpu.edi, cpu.eax
+    return b'\x97'
 
-byte_generators = gen_byte_generator()
-
+byte_generators = gen_byte_generators()
 def set_eax(n, prev=None):
-    res=b''
+    res = b''
     if prev is None:
+        # res += zero_eax()
         n ^= cpu.eax
     else:
         n ^= prev
-    
+    b0, b1, b2, b3 = (n >> 0) & 0xFF, (n >> 8) & 0xFF, (n >> 16) & 0xFF, (n >> 24) & 0xFF
+    g0, g1, g2, g3 = byte_generators[b0], byte_generators[b1], byte_generators[b2], byte_generators[b3]
+    for a, b, c, d in zip(g0, g1, g2, g3):
+        res += xor_eax((a << 0) + (b << 8) + (c << 16) + (d << 24))
+    return res
 
-def set_ebx(val):
-    pass
+def zero_edi():
+    return zero_eax() + xchg_eax_edi()
 
-def set_edi(addr):
-    pass
+def set_edi(n, prev=None):
+    return set_eax(n, prev) + xchg_eax_edi()
 
+def mov_ebx_ptr(addr):
+    # shit, no ebx emul
+    return set_edi(addr) + b'\x8b\x1f'
+
+def add_ebx_ptr(addr):
+    # shit, no ebx emul
+    return set_edi(addr) + b'\x03\x1f'
+
+def zero_ebx():
+    # shit, no ebx emul
+    for x in bin_consts:
+        if x[1] == 0:
+            return mov_ebx_ptr(x[0])
+
+
+def set_ebx(n, max_iters=1000):
+    q=[]
+    was=set()
+    for x in bin_consts:
+        if n>x[1]:
+            heappush(q,(n-x[1],(x,)))
+    res, iters=None, 0
+    while q and iters < max_iters:
+        iters+=1
+        now_sum, now=heappop(q)
+        now_sum = n-now_sum
+        if now_sum == n and (res is None or len(now) < len(res)):
+            res=now
+        was.add(now_sum)
+        for x in bin_consts:
+            if now_sum + x[1] <= n and now_sum + x[1] not in was:
+                heappush(q,(n-now_sum-x[1], now+(x,)))
+    return zero_ebx()+b''.join(add_ebx_ptr(x[0]) for x in res)
+            
 # \x89\x1f: mov dword ptr [rdi], ebx
 
 def patch_dword(addr, val):
@@ -102,8 +141,11 @@ def main():
         second_stage += b"\x90"
     first_stage=b''
 
+    for i in range(0,len(second_stage),4):
+        first_stage += patch_dword(0x1337c00 + i, u32(second_stage[i:i+4]))
+    
     with open("/tmp/dump.bin","wb") as f:
-        f.write(second_stage)
+        f.write(first_stage)
 
     '''
     for i in range(0, len(second_stage),4):
